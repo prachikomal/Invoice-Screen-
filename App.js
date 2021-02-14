d028709dd275ddbbf88ccc9763be968ea33cad0d
Prachi Komal <prachikkarn2015@gmail.com>
2021-02-14 13:26:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InvoiceScreen from './Veiws/InvoiceScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <InvoiceScreen />
+    </div>
+  );
+}
+
+export default App;
